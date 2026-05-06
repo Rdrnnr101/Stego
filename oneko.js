ecdb35ee6aed88fb7e49fd80f38295a135eb2ef3
Cranch <alexanderv@gmail.com>
2026-05-06 19:00:25 -0400

    Add files via upload

--- oneko.js
+++ oneko.js
@@ -96,7 +96,7 @@
     nekoEl.style.left = `${nekoPosX - spriteSize / 2}px`;
 	nekoEl.style.top = `${nekoPosY - spriteSize / 2}px`;
     nekoEl.style.zIndex = 2147483647;
-	nekoEl.style.transform = "scale(0.38)"; 
+	nekoEl.style.transform = "scale(0.7)"; 
 	
     let nekoFile = "./oneko.gif"
     const curScript = document.currentScript
